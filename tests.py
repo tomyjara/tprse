604b101e70afcd4936572c200d0d1925f89a4482
tomasj <tomas.jaratz@rappi.com>
2020-03-25 21:52:20 -0300

    message

--- tests.py
+++ tests.py
@@ -21,18 +21,18 @@ class TestModelos(unittest.TestCase):
 
     def iterarModeloSIR(self, cantidad_de_itaraciones, modelo):
         for i in range(cantidad_de_itaraciones):
-            iterar_SIR(modelo)
+            iterar_SIR(modelo, self.ti)
 
     def iterarModeloSIRS(self, cantidad_de_itaraciones, modelo):
         for i in range(cantidad_de_itaraciones):
-            iterar_SIRS(modelo)
+            iterar_SIRS(modelo, self.ti, self.ri)
 
     def instanciarModelo(self):
         return crearModelo(self.tipo_de_grafo, self.cantidad_de_nodos, self.probabilidad_de_infeccion, self.ti, self.ri)
 
     def test_correr_SIS_sin_nodos_infectados_no_debe_alterar_el_estado_de_ninguno(self):
 
-        self.cantidad_de_nodos = 2
+        self.cantidad_de_nodos = 4
         self.ti = 4
         cantidad_de_iteraciones = 4
 
@@ -44,7 +44,7 @@ class TestModelos(unittest.TestCase):
 
     def test_luego_de_2_iteraciones_de_SIS_con_ti_4_comenzando_con_todos_los_nodos_infectados_deben_seguir_todos_infectados(self):
 
-        self.cantidad_de_nodos = 2
+        self.cantidad_de_nodos = 4
         self.ti = 4
         self.probabilidad_de_infeccion = 1
 
@@ -58,7 +58,7 @@ class TestModelos(unittest.TestCase):
 
     def test_luego_de_4_iteraciones_de_SIS_con_ti_4_comenzando_con_todos_los_nodos_infectados_deben_terminar_todos_susceptibles(self):
 
-        self.cantidad_de_nodos = 2
+        self.cantidad_de_nodos = 4
         self.ti = 4
         self.probabilidad_de_infeccion = 1
 
@@ -69,3 +69,29 @@ class TestModelos(unittest.TestCase):
 
         for nodo in modelo.nodes:
             self.assertEqual(modelo.nodes[nodo]['estado'], S)
+
+    def test_correr_SIR_sin_nodos_infectados_no_debe_alterar_el_estado_de_ninguno(self):
+
+        self.cantidad_de_nodos = 4
+        self.ti = 4
+        cantidad_de_iteraciones = 4
+
+        modelo = self.instanciarModelo()
+        self.iterarModeloSIR(cantidad_de_iteraciones, modelo)
+
+        for nodo in modelo.nodes:
+            self.assertEqual(modelo.nodes[nodo]['estado'], S)
+
+    def test_luego_de_2_iteraciones_de_SIR_con_ti_4_comenzando_con_todos_los_nodos_infectados_deben_seguir_todos_infectados(self):
+
+        self.cantidad_de_nodos = 4
+        self.ti = 4
+        self.probabilidad_de_infeccion = 1
+
+        cantidad_de_iteraciones = 2
+
+        modelo = self.instanciarModelo()
+        self.iterarModeloSIR(cantidad_de_iteraciones, modelo)
+
+        for nodo in modelo.nodes:
+            self.assertEqual(modelo.nodes[nodo]['estado'], I)
